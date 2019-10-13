d1ccd5f9b3b54a1799245730b32442eacf4ee916
Anand Sharma <anandsharma110397@gmail.com>
2019-10-13 23:41:05 +0530

    version 1.1

---
+++ includes/blog.inc.php
@@ -0,0 +1,20 @@
+<div class="card my-3">
+    <div class="card-header bg-white">
+        <a class="text-dark text-decoration-none" href="http://">
+            <h5 class="mb-0">Heading of post(if any)</h5>
+        </a>
+    </div>
+    <div class="card-body">
+        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error perferendis veniam mollitia, ex laudantium debitis doloribus iste ipsa itaque quaerat? Dolore porro quo reprehenderit tenetur maiores placeat mollitia harum eum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error perferendis veniam mollitia, ex laudantium debitis doloribus iste ipsa itaque quaerat? Dolore porro quo reprehenderit tenetur maiores placeat mollitia harum eum.</p> -->
+        <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, provident dolorum numquam quasi reprehenderit libero, dolore nostrum earum error, at odit aut! Cumque excepturi reiciendis vitae rem eum et voluptate....</p>
+    </div>
+    <div class="card-footer bg-white d-flex">
+        <div class="mr-3">
+            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" class="img-fluid rounded-pill" style="width: 35px !important;">
+        </div>
+        <div>
+        <h6 class="card-title mb-0">Author Name</h6>
+        <p class="text-secondary mb-0 f-12">Date Posted</p>
+        </div>
+    </div>
+</div>
---
+++ includes/nav.php
@@ -0,0 +1,49 @@
+<style>
+ /* .nav-item{
+     margin: auto 10px;
+ } */
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm mb-4 bg-white">
+    <div class="container">
+    <a class="navbar-brand" href="#">Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <!-- <li class="nav-item">
+        <a class="nav-link font-weight-bolder" href="#">Login</a>
+      </li> -->
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+      <!-- <li class="nav-item">
+        <a class="btn btn-outline-success" href="#" >Get started</a>
+      </li> -->
+
+      <!-- after Login -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle font-weight-bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          +
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Add Blog</a>
+          <a class="dropdown-item" href="#">Add Image</a>
+          <!-- <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a> -->
+        </div>
+      </li>
+    </ul>
+  </div>
+    </div>
+</nav>
