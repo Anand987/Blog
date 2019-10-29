967aef2fdc99279f12303f5524d2ad439d3b1554
Anand Sharma <anandsharma110397@gmail.com>
2019-10-30 00:02:38 +0530

    minor changes

---
+++ about.php
@@ -0,0 +1,73 @@
+<?php
+include "includes/header.php";
+?>
+<style>
+    .bg-image1 {
+        background-image: url(./assets/images/aboutus.jpg) !important;
+        background-color: rgba(0, 0, 0, 0.68);
+        background-position: center !important;
+        background-size: cover !important;
+        background-repeat: no-repeat;
+        background-blend-mode: overlay;
+    }
+</style>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="jumbotron p-4 p-md-5 text-white rounded bg-image1 w-100" style="height: 50vh;">
+            <div class="col-md-6 offset-1 px-0">
+                <h1 class="display-4 font-italic">ABOUT <span class="font-weight-bold" style="color: #00E676;">EDU</span> BLOG</h1>
+                <p class="lead my-3">This is a platform for educational purpose in use of college students via college only</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container my-3">
+    <div class="row align-items-center">
+        <div class="col-sm-8">
+            <h2 class="font-weight-bold text-danger mb-5">ABOUT US</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis quia perferendis ex necessitatibus totam ducimus,
+                accusantium nihil, soluta a praesentium alias. Sapiente, qui. Amet aliquid soluta deserunt neque eius.
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis quia perferendis ex necessitatibus totam ducimus,
+                accusantium nihil, soluta a praesentium alias. Sapiente, qui. Amet aliquid soluta deserunt neque eius.
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis quia perferendis ex necessitatibus totam ducimus,
+                accusantium nihil, soluta a praesentium alias. Sapiente, qui. Amet aliquid soluta deserunt neque eius.
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <img class="img-fluid" src="assets/images/image2.jpg" alt="aboutimg">
+        </div>
+    </div>
+</div>
+<div class="container my-3">
+    <div class="row align-items-center">
+        <div class="col-sm-4">
+            <img class="img-fluid" src="assets/images/image2.jpg" alt="aboutimg">
+        </div>
+        <div class="col-sm-8">
+        <h2 class="font-weight-bold text-danger mb-5">OUR VISION</h2>
+            <p class="text-justify">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis quia perferendis ex necessitatibus totam ducimus,
+                accusantium nihil, soluta a praesentium alias. Sapiente, qui. Amet aliquid soluta deserunt neque eius.
+            </p>
+            <p class="text-justify">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis quia perferendis ex necessitatibus totam ducimus,
+                accusantium nihil, soluta a praesentium alias. Sapiente, qui. Amet aliquid soluta deserunt neque eius.
+            </p>
+            <p class="text-justify">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia blanditiis quia perferendis ex necessitatibus totam ducimus,
+                accusantium nihil, soluta a praesentium alias. Sapiente, qui. Amet aliquid soluta deserunt neque eius.
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+include "includes/footer.php";
+?>
--- viewblog.php
+++ viewblog.php
@@ -1,82 +1,100 @@
 <?php
-    require "includes/dbh.inc.php";
-    include "includes/header.php";
-    $title = $_GET['title'];
-    $author = $_GET['author'];
+require "includes/dbh.inc.php";
+include "includes/header.php";
+$title = $_GET['title'];
+$author = $_GET['author'];
 
-    $sql = "SELECT * FROM usersblogs INNER JOIN users ON usersblogs.u_id LIKE users.u_id;";
+$sql = "SELECT * FROM usersblogs INNER JOIN users ON usersblogs.u_id LIKE users.u_id;";
 
-    $sql = "SELECT * FROM usersblogs WHERE b_title=?";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt,$sql)) {
-        header("Location: ../viewblog.php?error=sqlerror");
-        exit();
-    }
-    else {
-        mysqli_stmt_bind_param($stmt, "s", $title);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        if ($row = mysqli_fetch_assoc($result)) {
-            $startdate = strtotime(str_replace('-','/', $row['b_date']));
-?>
+$sql = "SELECT * FROM usersblogs WHERE b_title=?";
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header("Location: ../viewblog.php?error=sqlerror");
+    exit();
+} else {
+    mysqli_stmt_bind_param($stmt, "s", $title);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
+        $startdate = strtotime(str_replace('-', '/', $row['b_date']));
+        ?>
 
-<div class="container mt-3">
-    <div class="card">
-        <div class="card-header">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-9">
-                        <?php
-                            echo '
-                            <h5 class="mb-0">'. $row['b_title'] .'</h5>
-                            <h6 class="mb-0">'. $row['b_subtitle'] .' <span class="badge badge-secondary"> </span></h6>
+        <div class="container mt-3" style="min-height: 500px;">
+            <div class="card">
+                <div class="card-header bg-primary text-light">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-9">
+                                <?php
+                                        echo '
+                            <h5 class="mb-0">' . $row['b_title'] . '</h5>
+                            <h6 class="mb-0">' . $row['b_subtitle'] . '</h6>
                             ';
-                        ?>
-                    </div>
-                    <div class="col-3">
-                            <?php
-                                if (isset($_SESSION['user_id'])) {
-                                    ?>
-                                    
+                                        ?>
+                            </div>
+                            <div class="col-3">
+                                <?php
+                                        if (isset($_SESSION['user_id'])) {
+                                            ?>
+
                                     <div class="text-right">
-                                    <form  class="d-inline" action="includes/update.inc.php" method="post">
-                                        <button class="btn btn-sm btn-warning text-uppercase" type="submit" name="update-blog">update</button>
-                                    </form>
-                                    <form  class="d-inline" action="includes/delete.inc.php" method="post">
-                                        <button class="btn btn-sm btn-danger text-uppercase" type="submit" name="delete-blog">delete</button>
-                                    </form>
+                                        <form class="d-inline" action="includes/update.inc.php" method="post">
+                                            <button class="btn btn-sm btn-warning text-uppercase" type="submit" name="update-blog">update</button>
+                                        </form>
+                                        <form class="d-inline" action="includes/delete.inc.php" method="post">
+                                            <button class="btn btn-sm btn-danger text-uppercase" type="submit" name="delete-blog">delete</button>
+                                        </form>
                                     </div>
-                                    
-                                    <?php
-                                }
-                                else {
-                                }
-                            ?>
+
+                                <?php
+                                        } else { }
+                                        ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div class="card-body">
+                    <?php
+                            echo '
+                            <p>' . $row['b_content'] . '</p>
+                            <div class="blockquote-footer">' . $author . ' <cite title="Source Title">' . date("M d", $startdate) . '</cite></div>
+                            ';
+                            ?>
+                </div>
+
+                <div class="card-footer bg-light text-dark">
+                    <!-- show comments from database-->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-1 py-3">
+                                <img class="img-fluid" src="assets/images/logo.png" alt="profile image">
+                            </div>
+                            <div class="col-sm-9 py-3">
+                                <span class="font-weight-bold">Username</span> <span class="text-muted small">Time Uploaded</span>
+                                <div>
+                                    content
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-1 py-3">
+                                <img class="img-fluid" src="assets/images/logo.png" alt="profile image">
+                            </div>
+                            <div class="col-10 py-3">
+                                <!-- form to post comments -->
+                                <form action="" method="post">
+                                    <input class="form-control" type="text" name="commentBrief" id="Comment">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
-        <div class="card-body">
-        <?php
-                            echo '
-                            <p>'. $row['b_content'] .'</p>
-                            <div class="blockquote-footer">'. $author .' <cite title="Source Title">'. date("M d" , $startdate) .'</cite></div>
-                            ';
-                        ?>
-        </div>
 
-        <div class="card-footer">
-            <!-- show comments from database-->
-                
-            <!-- form to post comments -->
-            <form action="" method="post"></form>
-        </div>
-
-    </div>
-</div>
-
-<?php          
-        }
+<?php
     }
-    include "includes/footer.php"
+}
+include "includes/footer.php"
 ?>
